d92a5fdfda36c937abb8c4ac54c512b80ab4eae7
RezaRahimi166 <rezaa.salami99@gmail.com>
2024-10-22 04:37:36 -0800

    feat(postslug): creating dymic routes and page for posts

--- src/app/blogs/loading.jsx
+++ src/app/blogs/loading.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Loading = () => {
   return (
     <div className="flex flex-col items-center gap-x-4">
-      <span className="text-lg text-secondary-500">درحال بارگذاری پست ها</span>
+      <span className="text-lg text-secondary-500">درحال بارگذاری اطلاعات</span>
       <Spinner />
     </div>
   );
