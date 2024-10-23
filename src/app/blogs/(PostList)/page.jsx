40f9ddca73deb77b1d8e82e7e2d8d207e044dcca
RezaRahimi166 <rezaa.salami99@gmail.com>
2024-10-23 04:51:12 -0800

    feat(data caching): setting up

--- src/app/blogs/(PostList)/page.jsx
+++ src/app/blogs/(PostList)/page.jsx
@@ -2,6 +2,16 @@ import { Suspense } from "react";
 import PostList from "../_components/PostList";
 import Spinner from "@/ui/Spinner";
 
+// how to revalidate, time-based :
+
+// export const revalidate = 0; // opt out => dynamic (!!!)
+// export const dynamic = "force-dynamic"; // opt out => dynamic (!!!)
+
+export const revalidate = 15; // after 15 => re-build =>
+//  1. pass time interval
+//  + 2.new incoming request to build thes page =>
+// updated data will be shown to next user !! =>
+// ISR => Incermental Static re-generetion
 const BlogPage = () => {
   return (
     <div>
