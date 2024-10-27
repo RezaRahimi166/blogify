28aa7b4db60eee8792da1ae2be01421b8744641a
RezaRahimi166 <rezaa.salami99@gmail.com>
2024-10-27 12:19:42 -0800

    feat(panel): fetching data panel for number of users && comments && posts

--- src/app/(dashboard)/profile/page.jsx
+++ src/app/(dashboard)/profile/page.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { fetchCardData } from "@/services/data";
+import { Card } from "./_components/Cards";
 
-const page = () => {
-  return <div>page</div>;
+const Profile = async () => {
+  const { numberOfComments, numberOfPosts, numberOfUsers } =
+    await fetchCardData();
+
+  return (
+    <div>
+      <div className="grid gap-6 md:grid-cols-3 mb-8 ">
+        <Card title={"کاربران"} value={numberOfUsers} type="users" />
+        <Card title={"پست ها"} value={numberOfPosts} type="posts" />
+        <Card title={"نظرات"} value={numberOfComments} type="comments" />
+      </div>
+    </div>
+  );
 };
 
-export default page;
+export default Profile;
