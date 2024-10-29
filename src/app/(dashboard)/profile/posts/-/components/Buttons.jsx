43c67d93da25140e6258020b39d3373e8100b163
RezaRahimi166 <rezaa.salami99@gmail.com>
2024-10-29 04:52:52 -0800

    feat(pane;l): adding search logic

--- src/app/(dashboard)/profile/posts/-/components/Buttons.jsx
+++ src/app/(dashboard)/profile/posts/-/components/Buttons.jsx
@@ -2,7 +2,23 @@
 import Link from "next/link";
 
 const { default: ButtonIcon } = require("@/ui/ButtonIcon");
-const { TrashIcon, PencilIcon } = require("@heroicons/react/24/outline");
+const {
+  TrashIcon,
+  PencilIcon,
+  PlusIcon,
+} = require("@heroicons/react/24/outline");
+
+export function CreatePost() {
+  return (
+    <Link
+      href={"/profile/posts/create"}
+      className="justify-self-end gap-x-4 flex py-3 items-center  rounded-lg bg-primary-900 px-4 text-sm font-medium transition-colors hover:bg-primary-700 "
+    >
+      <span className="hidden md:block">ایجاد پست</span>
+      <PlusIcon className="w-5" />
+    </Link>
+  );
+}
 
 const DeletePost = ({ id }) => {
   return (
--- src/app/(dashboard)/profile/posts/page.jsx
+++ src/app/(dashboard)/profile/posts/page.jsx
@@ -1,13 +1,26 @@
 import { Suspense } from "react";
 import PostTable from "./-/components/PostTable";
 import Fallback from "@/ui/Fallback";
+import Search from "@/ui/Search";
+import { CreatePost } from "./-/components/Buttons";
+import queryString from "query-string";
+import { getPosts } from "@/services/postServices";
 
-const Posts = () => {
+const Posts = async ({ searchParams }) => {
+  const query = queryString.stringify(searchParams);
+  const { totalPages } = await getPosts(query);
+
   return (
     <div>
-      <Suspense fallback={<Fallback />}>
-        <PostTable />
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 items-center text-secondary-700 mb-12">
+        <h1 className="font-bold text-xl">لیست پست ها</h1>
+        <Search />
+        <CreatePost />
+      </div>
+      <Suspense fallback={<Fallback />} key={query}>
+        <PostTable query={query} />
       </Suspense>
+      <div className="flex justify-center mt-5 w-full "></div>
     </div>
   );
 };
