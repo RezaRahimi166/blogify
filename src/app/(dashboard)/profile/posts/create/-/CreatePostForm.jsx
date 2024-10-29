091f70b6172a664e5195a39e026555a5a27df532
RezaRahimi166 <rezaa.salami99@gmail.com>
2024-10-29 06:54:05 -0800

    feat(panel): create form image uploader

--- src/app/(dashboard)/profile/posts/create/-/CreatePostForm.jsx
+++ src/app/(dashboard)/profile/posts/create/-/CreatePostForm.jsx
@@ -1,21 +1,29 @@
 "use client";
 import { useCategories } from "@/hooks/useCategories";
+import ButtonIcon from "@/ui/ButtonIcon";
 import RHFSelect from "@/ui/RHFSelect";
 import RHFTextField from "@/ui/RHFTextField";
+import TextField from "@/ui/TextField";
+import { XMarkIcon } from "@heroicons/react/24/outline";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { useForm } from "react-hook-form";
+import Image from "next/image";
+import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
 import * as yup from "yup";
 
 const schema = yup.object();
 
 const CreatePostForm = () => {
   const { categories } = useCategories();
+  const [coverImageUrl, setCoverImageUrl] = useState(null);
 
   const {
     register,
     handleSubmit,
     reset,
+    control,
     formState: { errors },
+    setValue,
   } = useForm({
     mode: "onTouched",
     resolver: yupResolver(schema),
@@ -66,6 +74,51 @@ const CreatePostForm = () => {
         isRequired
         options={categories}
       />
+
+      <Controller
+        name="coverImage"
+        control={control}
+        rules={{ required: "کاور چست الزامی است" }}
+        render={({ field: { value, onChange, ...rest } }) => {
+          return (
+            <TextField
+              type="file"
+              isRequired
+              name={"my-coverImage"}
+              label={"کاور پست"}
+              {...rest}
+              value={value?.fileName}
+              onChange={(event) => {
+                const file = event.target.files[0];
+                onChange(file);
+                setCoverImageUrl(URL?.createObjectURL(file));
+                event.target.value = null;
+              }}
+            />
+          );
+        }}
+      />
+      {coverImageUrl && (
+        <div className="relative aspect-video overflow-hidden rounded-lg">
+          <Image
+            fill
+            alt="cover-image"
+            src={coverImageUrl}
+            className="object-cover object-center"
+          />
+
+          <ButtonIcon
+            variant={"red"}
+            className={"w-6 h-6 absolute left-4 top-4"}
+            onClick={() => {
+              setCoverImageUrl(null);
+              setValue("coverImage", null);
+            }}
+          >
+            <XMarkIcon />
+          </ButtonIcon>
+        </div>
+      )}
     </form>
   );
 };
